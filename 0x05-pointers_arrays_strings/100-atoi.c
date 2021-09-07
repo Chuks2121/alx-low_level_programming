99479d0a427b97d097176f035c219598ba8fe7e5
chuks2121 <chukylofashionstar@gmail.com>
2021-09-06 22:43:45 -0700

    my commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,7 +7,7 @@
 */
 int _atoi(char *s)
 {
-unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+unsigned int count = 0, size = 0, oi = 0, bar = 1, m = 1, i;
 
 while (*(s + count) != '\0')
 {
@@ -15,7 +15,7 @@ if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 break;
 
 if (*(s + count) == '-')
-pn *= -1;
+bar *= -1;
 
 if ((*(s + count) >= '0') && (*(s + count) <= '9'))
 {
@@ -28,8 +28,8 @@ count++;
 
 for (i = count - size; i < count; i++)
 {
-oi = oi + ((*(s + i) - 48) * m);
+foo = foo + ((*(s + i) - 48) * m);
 m /= 10;
 }
-return (foo * bar);
+return (foo *bar);
 }
